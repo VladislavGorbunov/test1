a20567e13f567e55d9450d2d9ee54063801e0e67
unknown <limitorg2016@ya.ru>
2024-05-25 09:45:41 +0300

    first commit

--- test1/index.php
+++ test1/index.php
@@ -19,38 +19,3 @@ if ($redis->get('start') == true) {
 }
 
 
-// class TestOne 
-// {
-//     private $timeSleep = 5;
-//     private static $redisHost = '127.0.0.1';
-//     private static $redisPort = 6379;
-//     public $redisx;
-
-//     public function __construct() 
-//     {
-
-//     }
-
-//     public function redisConnect()
-//     {
-//         $this->redisx = new Redis();
-//         $this->redisx->connect(self::$redisHost, self::$redisPort);
-//     }
-
-//     public function rSet($key, $value) 
-//     {
-//         $this->redisx->set($key, $value);
-//     }
-
-//     public function rGet($key) 
-//     {
-//         return $this->redisx->get($key);
-//     }
-
-// }
-
-// $r = new TestOne();
-// $r->redisConnect();
-// $r->rSet('key', 123);
-// echo $r->rGet('key');
-
